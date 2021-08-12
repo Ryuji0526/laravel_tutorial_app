13a892db593ace22e6487550ca804601d769106e
Ryuji <giants0526@icloud.com>
2021-08-13 01:23:33 +0900

    [追加]Postの作成

---
+++ resources/views/front/posts/index.blade.php
@@ -0,0 +1,25 @@
+<?php
+$title = '投稿一覧';
+?>
+@extends('front.layouts.base')
+
+@section('content')
+<div class="card-header">{{ $title }}</div>
+<div class="card-body">
+    @if($posts->count() <= 0)
+        <p>表示する投稿はありません。</p>
+    @else
+        <table class="table">
+            @foreach($posts as $post)
+                <tr>
+                      <td>{{ $post->published_format }}</td>
+                      <td>{!! link_to_route('front.posts.show', $post->title, $post) !!}</td>
+                </tr>
+            @endforeach
+        </table>
+        <div class="d-flex justify-content-center">
+            {{ $posts->links() }}
+        </div>
+    @endif
+</div>
+@endsection
